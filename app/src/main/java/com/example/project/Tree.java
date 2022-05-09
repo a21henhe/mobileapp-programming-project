a78c731b42b5d7a880dd944ec02d8e471c5af5bd
a21henhe <a21henhe@student.his.se>
2022-05-09 15:16:11 +0200

    added a class tree

---
+++ app/src/main/java/com/example/project/Tree.java
@@ -0,0 +1,40 @@
+package com.example.project;
+
+public class Tree {
+
+    private int id;
+    private String name;
+    private int height;
+
+    public Tree(int id, String name, int height) {
+        this.id = id;
+        this.name = name;
+        this.height = height;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+
+}
