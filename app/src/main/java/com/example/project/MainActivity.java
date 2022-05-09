861ba965319fef3aa5d393bd6d72ada3f8a1aaae
a21henhe <a21henhe@student.his.se>
2022-05-09 15:41:24 +0200

    added recyclerviewadapter and connected to list of trees

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    RecyclerviewAdapter adapter;
+    ArrayList<Tree> listOfTrees;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +25,23 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         recyclerView = findViewById(R.id.recyclerView);
+        listOfTrees = new ArrayList<>(Arrays.asList(
+                new Tree(1, "ek", 40),
+                new Tree(2, "björk", 30),
+                new Tree(3, "asp", 25)
+        ));
+
+
+        adapter = new RecyclerviewAdapter(this, listOfTrees, new RecyclerviewAdapter.OnClickListener() {
+            @Override
+            public void onClick(Tree tree) {
+                Toast.makeText(MainActivity.this, tree.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-       // recyclerView.setAdapter(adapter);
+        recyclerView.setAdapter(adapter);
     }
 
 }
---
+++ app/src/main/java/com/example/project/RecyclerviewAdapter.java
@@ -0,0 +1,61 @@
+package com.example.project;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerviewAdapter extends RecyclerView.Adapter<RecyclerviewAdapter.ViewHolder> {
+
+    private List<Tree> listOfTrees;
+    private LayoutInflater layoutInflater;
+    private OnClickListener onClickListener;
+
+
+    RecyclerviewAdapter(Context context, List<Tree> trees,OnClickListener onClickListener){
+        this.layoutInflater = LayoutInflater.from(context);
+        this.listOfTrees = trees;
+        this.onClickListener = onClickListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(layoutInflater.inflate(R.layout.tree_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerviewAdapter.ViewHolder holder, int position) {
+        holder.name.setText(listOfTrees.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listOfTrees.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView name;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            name = itemView.findViewById(R.id.tree_name);
+        }
+
+        @Override
+        public void onClick(View view) {
+            onClickListener.onClick(listOfTrees.get(getAdapterPosition()));
+        }
+    }
+
+    public interface OnClickListener{
+        void onClick(Tree tree);
+    }
+}
